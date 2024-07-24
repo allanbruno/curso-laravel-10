01bc1418f1de5555bdd522e9ceb254b76301d622
Allan <allanbarbosa@prologapp.com>
2024-07-24 20:00:55 -0300

    feat: cria listagem com dados retornados do banco de dados

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Produto;
 use Illuminate\Http\Request;
 
 class ProdutoController extends Controller
 {
     public function index()
     {
-        return 'produtos';
+        $findProdutos = Produto::all();
+
+        return view('pages.produtos.paginacao', compact('findProdutos'));
     }
 }
