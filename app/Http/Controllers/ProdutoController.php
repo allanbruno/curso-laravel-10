aea34eaa57f9c75e92f58c25533cb854e0587b36
Allan <allanbarbosa@prologapp.com>
2024-07-25 11:22:23 -0300

    feat: cria espaço para ações

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -4,12 +4,20 @@ namespace App\Http\Controllers;
 
 use App\Models\Produto;
 use Illuminate\Http\Request;
+use function Laravel\Prompts\search;
 
 class ProdutoController extends Controller
 {
-    public function index()
+
+    public function __construct(Produto $produto)
     {
-        $findProdutos = Produto::all();
+        $this->produto = $produto;
+    }
+
+    public function index(Request $request)
+    {
+        $pesquisar = $request->pesquisar;
+        $findProdutos = $this->produto->getProdutosPesquisarIndex(search: $pesquisar ?? '');
 
         return view('pages.produtos.paginacao', compact('findProdutos'));
     }
