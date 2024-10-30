ac2a474a941efcb456ff1080c21d6487d71152b9
Allan <allanbarbosa@prologapp.com>
2024-10-29 21:15:46 -0300

    feat: aplica seeder de vendas

--- database/seeders/VendasSeeder.php
+++ database/seeders/VendasSeeder.php
@@ -2,13 +2,24 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Venda;
 use Illuminate\Database\Seeder;
 
 class VendasSeeder extends Seeder
 {
     public function run(): void
     {
-
+        Venda::create(
+            [
+                'numero_da_venda' => 1,
+                'produto_id' => 6,
+                'cliente_id' => 3
+            ],
+            [
+                'numero_da_venda' => 2,
+                'produto_id' => 8,
+                'cliente_id' => 5
+            ]
+        );
     }
 }
