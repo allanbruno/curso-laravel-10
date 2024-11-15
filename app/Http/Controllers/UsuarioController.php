ba5944eb88b1f7e7e415bc054cfac81a53ae9395
Allan <allanbarbosa@prologapp.com>
2024-11-15 14:05:45 -0300

    feat: aplica busca de usuários

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -2,14 +2,64 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UsuarioController extends Controller
 {
 
-    public function index()
+    public function __construct(User $user)
     {
-        return 'teste';
+        $this->user = $user;
+    }
+
+    public function index(Request $request)
+    {
+        $pesquisar = $request->pesquisar;
+        $findUsuario = $this->user->getUsuariosPesquisarIndex(search: $pesquisar ?? '');
+
+        return view('pages.usuario.paginacao', compact('findUsuario'));
+    }
+
+    public function delete(Request $request)
+    {
+        $id = $request->id;
+        $buscarRegistro = User::find($id);
+        $buscarRegistro->delete();
+
+        return response()->json(['success' => true]);
+    }
+
+    public function cadastrarUsuario(Request $request)
+    {
+        if ($request->method() == "POST") {
+            $data = $request->all();
+            User::create($data);
+
+            toastr()->success('Usuário cadastrado com sucesso!');
+
+            return redirect()->route('usuario.index');
+        }
+
+        return view('pages.usuario.create');
+    }
+
+    public function atualizarUsuario(Request $request, $id)
+    {
+        if ($request->method() == "PUT") {
+            $data = $request->all();
+
+            $buscaRegistro = User::find($id);
+            $buscaRegistro->update($data);
+
+            toastr()->success('Usuário alterado com sucesso!');
+
+            return redirect()->route('usuario.index');
+        }
+
+        $findUsuario = User::where('id', $id)->first();
+
+        return view('pages.usuario.atualiza', compact('findUsuario'));
     }
 
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -25,4 +25,16 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function getUsuariosPesquisarIndex(string $search = '')
+    {
+        $usuario = $this->where(function ($query) use ($search) {
+            if ($search) {
+                $query->where('nome', $search);
+                $query->orWhere('nome', 'LIKE', "%{$search}%");
+            }
+        })->get();
+
+        return $usuario;
+    }
 }
